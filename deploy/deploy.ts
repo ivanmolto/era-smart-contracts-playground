a4a7bf4159317dbdf6415e0efee6dd93e11391fc
Ivan Molto <ivanmolto@gmail.com>
2023-03-12 22:49:31 +0100

    chore: Add deployment

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,60 +1,45 @@
 import { delay } from '@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService';
 import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { IRMRKInitData } from '../typechain-types/contracts/SimpleEquippable';
 import { run } from 'hardhat';
-import { SimpleEquippable } from '../typechain-types';
+import { Film, Character } from '../typechain-types';
 import { Wallet, utils } from 'zksync-web3';
 import * as ethers from 'ethers';
 
 export default async function (hre: HardhatRuntimeEnvironment) {
-  console.log('Deploying smart simple equippable contract');
+  console.log('Deploying smart film contract');
 
   const wallet = new Wallet(process.env.PRIVATE_KEY || '');
 
   // Create deployer object and load the artifact of the contract you want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact('SimpleEquippable');
-
-  const initData: IRMRKInitData.InitDataStruct = {
-    erc20TokenAddress: ethers.constants.AddressZero,
-    tokenUriIsEnumerable: false,
-    royaltyRecipient: ethers.constants.AddressZero,
-    royaltyPercentageBps: 1000,
-    maxSupply: ethers.BigNumber.from(1000),
-    pricePerMint: ethers.utils.parseEther('1.0'),
-  };
-
-  const args = ['Kanaria', 'KAN', 'ipfs://collectionMeta', 'ipfs://tokenMeta', initData];
+  const filmArtifact = await deployer.loadArtifact('Film');
+  const filmArgs = ['Pet Projects', 'PET', 10_000];
+  // const filmArgs = ['Charge', 'CHARGE', 10_000];
+  // const filmArgs = ['Sprite Fright', 'SPRITE', 10_000];
+  // const filmArgs = ['Coffee Run', 'COFFEE', 10_000];
+  // const filmArgs = ['Spring', 'SPRING', 10_000];
+  // const filmArgs = ['Caminandes Llamigos', 'CAMINANDES', 10_000];
 
   // Estimate contract deployment fee
-  const deploymentFee = await deployer.estimateDeployFee(artifact, args);
-
-  // OPTIONAL: Deposit funds to L2
-  // Comment this block if you already have funds on zkSync.
-  // const depositHandle = await deployer.zkWallet.deposit({
-  //   to: deployer.zkWallet.address,
-  //   token: utils.ETH_ADDRESS,
-  //   amount: deploymentFee,
-  // });
-  // // Wait until the deposit is processed on zkSync
-  // await depositHandle.wait();
+  const filmDeploymentFee = await deployer.estimateDeployFee(filmArtifact, filmArgs);
 
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  const parsedFee = ethers.utils.formatEther(filmDeploymentFee.toString());
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
-  const kanaria = <SimpleEquippable>await deployer.deploy(artifact, args);
+  const film = <Film>await deployer.deploy(filmArtifact, filmArgs);
 
-  await kanaria.deployed();
-  console.log(`Sample contracts deployed to ${kanaria.address}.`);
+  await film.deployed();
+
+  console.log(`Sample contracts deployed to ${film.address}.`);
 
   await delay(20000);
   console.log('Will verify now');
 
   await run(`verify:verify`, {
-    address: kanaria.address,
-    constructorArguments: args,
-    contract: 'contracts/SimpleEquippable.sol:SimpleEquippable',
+    address: film.address,
+    constructorArguments: filmArgs,
+    contract: 'contracts/Film.sol:Film',
   });
 }
